ad50d8360742a940c5d59cb673c345dbb279fe18
ilkukrisz <ilku355@gmail.com>
2018-11-20 12:37:54 +0100

    Route had been added to Board.

--- src/evoLudoMain/Board.java
+++ src/evoLudoMain/Board.java
@@ -6,11 +6,11 @@ import java.awt.*;
 
 public class Board extends JFrame {
 
-    private MyContainingJPanel myContainingJPanel = new MyContainingJPanel();
-    private JPanel redsYard = new JPanel();
-    private JPanel bluesYard = new JPanel();
-    private JPanel greensYard = new JPanel();
-    private JPanel yellowsYard = new JPanel();
+    private MyContainingJPanel myContainingJPanel = MyContainingJPanel.getInstance();
+    private YardPanels redsYard = new YardPanels();
+    private YardPanels bluesYard = new YardPanels();
+    private YardPanels greensYard = new YardPanels();
+    private YardPanels yellowsYard = new YardPanels();
     private JPanel homeYard = new JPanel();
     private JPanel yellowRoute = new JPanel();
     private JPanel greenRoute = new JPanel();
@@ -21,6 +21,7 @@ public class Board extends JFrame {
     private double HEIGHT = gd.getDisplayMode().getHeight();
     private int scaleUnitWidth = (int) (WIDTH / 5);
     private int scaleUnitHeight = (int) (HEIGHT / 5);
+    private Route route = Route.getInstance();
 
     public Board() {
         /**
@@ -58,16 +59,36 @@ public class Board extends JFrame {
         this.scaleUnitWidth = (int) (this.WIDTH / 5 * 2);
         this.scaleUnitHeight = (int) (this.HEIGHT / 5 * 2);
         myContainingJPanel.setSizes((int)this.WIDTH, (int)this.HEIGHT, this.scaleUnitWidth, this.scaleUnitHeight, shiftWidth, shiftHeight);
-        redsYard.setBounds((int) shiftWidth, (int) shiftHeight, this.scaleUnitWidth, this.scaleUnitHeight);
-        bluesYard.setBounds((int) (this.scaleUnitWidth /2*3 + shiftWidth),(int) ((0*HEIGHT) + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight);
-        greensYard.setBounds((int) (this.scaleUnitWidth /2*3 + shiftWidth), (int) (this.scaleUnitHeight /2*3 + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight);
-        yellowsYard.setBounds((int) ((int) (0*WIDTH) + shiftWidth), (int) (this.scaleUnitHeight /2*3 + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight);
-        homeYard.setBounds((int) (this.scaleUnitWidth + shiftWidth), (int) (this.scaleUnitHeight + shiftHeight), this.scaleUnitWidth /2, this.scaleUnitHeight /2);
-        redRoute.setBounds((int) shiftWidth, (int) (this.scaleUnitHeight + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight /2);
-        blueRoute.setBounds((int) (this.scaleUnitWidth + shiftWidth), (int) shiftHeight, this.scaleUnitWidth /2, this.scaleUnitHeight);
-        greenRoute.setBounds((int) ((this.scaleUnitWidth /2)*3 + shiftWidth), (int) (this.scaleUnitHeight + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight /2);
-        yellowRoute.setBounds((int) (this.scaleUnitWidth + shiftWidth), (int) ((this.scaleUnitHeight /2)*3 + shiftHeight), this.scaleUnitWidth /2, this.scaleUnitHeight);
 
+        //yards
+
+        redsYard.setBounds((int) shiftWidth, (int) shiftHeight, this.scaleUnitWidth, this.scaleUnitHeight);
+
+        bluesYard.setBounds((int) (this.scaleUnitWidth /2*3 + shiftWidth),(int) ((0*HEIGHT) + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight);
+
+        greensYard.setBounds((int) (this.scaleUnitWidth /2*3 + shiftWidth), (int) (this.scaleUnitHeight /2*3 + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight);
+
+        yellowsYard.setBounds((int) ((int) (0*WIDTH) + shiftWidth), (int) (this.scaleUnitHeight /2*3 + shiftHeight), this.scaleUnitWidth, this.scaleUnitHeight);
+
+        homeYard.setBounds((int) (this.scaleUnitWidth + shiftWidth), (int) (this.scaleUnitHeight + shiftHeight), this.scaleUnitWidth /2, this.scaleUnitHeight /2);
+
+        //routes
+
+        redRoute.setBounds((int) shiftWidth + scaleUnitWidth/6, (int) (this.scaleUnitHeight + shiftHeight) + scaleUnitWidth/6, this.scaleUnitWidth / 6 * 5, this.scaleUnitHeight / 6);
+
+        blueRoute.setBounds((int) (this.scaleUnitWidth + shiftWidth) + scaleUnitWidth/6, (int) shiftHeight + scaleUnitWidth/6, this.scaleUnitWidth / 6, this.scaleUnitHeight/ 6 *5);
+
+        greenRoute.setBounds((int) ((this.scaleUnitWidth /2)*3 + shiftWidth), (int) (this.scaleUnitHeight + shiftHeight) + scaleUnitWidth/6, this.scaleUnitWidth / 6 * 5, this.scaleUnitHeight /6);
+
+        yellowRoute.setBounds((int) (this.scaleUnitWidth + shiftWidth) + scaleUnitWidth/6, (int) ((this.scaleUnitHeight /2)*3 + shiftHeight) + scaleUnitWidth/6, this.scaleUnitWidth /6, this.scaleUnitHeight/6 * 5);
+
+
+        //Refreshing values to work dynamically
+        redsYard.refreshSizes();
+        bluesYard.refreshSizes();
+        greensYard.refreshSizes();
+        yellowsYard.refreshSizes();
+        route.resetPointPositions(scaleUnitWidth, scaleUnitHeight, shiftWidth, shiftHeight);
 
     }
     /**
@@ -78,21 +99,21 @@ public class Board extends JFrame {
          * Setting up the red player's yard, where the tokens stand.
          */
         redsYard.setBounds(0, 0, scaleUnitWidth, scaleUnitHeight);
-        redsYard.setBackground(new Color(255, 0, 0));
+        //redsYard.setBackground(new Color(255, 0, 0));
         redsYard.setVisible(true);
 
         /**
          * Setting up the blue player's yard, where the tokens stand.
          */
         bluesYard.setBounds(scaleUnitWidth /2*3, (int) (0*HEIGHT), scaleUnitWidth, scaleUnitHeight);
-        bluesYard.setBackground(new Color(0, 0, 255));
+        //bluesYard.setBackground(new Color(0, 0, 255));
         bluesYard.setVisible(true);
 
         /**
          * Setting up the green player's yard, where the tokens stand.
          */
         greensYard.setBounds(scaleUnitWidth /2*3, scaleUnitHeight /2*3, scaleUnitWidth, scaleUnitHeight);
-        greensYard.setBackground(new Color(0, 255, 0));
+        //greensYard.setBackground(new Color(0, 255, 0));
         greensYard.setVisible(true);
 
 
@@ -101,7 +122,7 @@ public class Board extends JFrame {
          * Setting up the yellow player's yard, where the tokens stand.
          */
         yellowsYard.setBounds((int) (0*WIDTH), scaleUnitHeight /2*3, scaleUnitWidth, scaleUnitHeight);
-        yellowsYard.setBackground(new Color(255, 255, 0));
+       // yellowsYard.setBackground(new Color(255, 255, 0));
         yellowsYard.setVisible(true);
 
 
@@ -115,25 +136,31 @@ public class Board extends JFrame {
          * The route under red's yard.
          */
         redRoute.setBounds(0, scaleUnitHeight, scaleUnitWidth, scaleUnitHeight /2);
+        redRoute.setBackground(new Color(255, 0, 0));
         redRoute.setVisible(true);
 
         /**
          * The route on the left of blue's yard.
          */
-        blueRoute.setBounds(scaleUnitWidth, 0, scaleUnitWidth /2, scaleUnitHeight);
+        blueRoute.setBounds(scaleUnitWidth, 0, scaleUnitWidth / 2, scaleUnitHeight);
+        blueRoute.setBackground(new Color(0, 0, 225));
         blueRoute.setVisible(true);
 
         /**
          * The route above green's yard.
          */
         greenRoute.setBounds((scaleUnitWidth /2)*3, scaleUnitHeight, scaleUnitWidth, scaleUnitHeight /2);
+        greenRoute.setBackground(new Color(0, 255, 0));
         greenRoute.setVisible(true);
 
         /**
          * The route on the right of yellow's yard.
          */
         yellowRoute.setBounds(scaleUnitWidth, (scaleUnitHeight /2)*3, scaleUnitWidth /2, scaleUnitHeight);
+        yellowRoute.setBackground(new Color(255, 255, 0));
         yellowRoute.setVisible(true);
+
+
 
         myContainingJPanel.setBounds(0, 0, (int)WIDTH, (int)HEIGHT);
         myContainingJPanel.setVisible(true);
@@ -152,16 +179,6 @@ public class Board extends JFrame {
         myContainingJPanel.add(greenRoute);
         myContainingJPanel.add(yellowRoute);
         myContainingJPanel.add(homeYard);
-        /*
-        add(redsYard);
-        add(bluesYard);
-        add(greensYard);
-        add(yellowsYard);
-        add(redRoute);
-        add(blueRoute);
-        add(greenRoute);
-        add(yellowRoute);
-        add(homeYard);*/
         add(myContainingJPanel);
 
 
---
+++ src/evoLudoMain/YardPanels.java
@@ -0,0 +1,98 @@
+package evoLudoMain;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+
+public class YardPanels extends JPanel {
+    private int[] xCoordinates = new int[4];
+    private int[] yCoordinates = new int[4];
+    private int ovalDiameter = 0;
+    private JButton diceRollerButton = new JButton("Dice me!");
+    private JLabel diceRollingResult = new JLabel();
+    private int panelWidth = (int) getSize().getWidth();
+    private int panelHeight = (int) getSize().getHeight();
+
+
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        //TODO Elődnek ide kell rajzolnia a köröket.
+    }
+
+
+    public YardPanels() {
+
+        setLayout(null);
+
+        diceRollerButton.setBounds(panelWidth / 10 , 5 , panelWidth/10 * 5, panelHeight / 10 );
+        diceRollingResult.setBounds(panelWidth / 10 * 8, 5, panelWidth / 10 * 3, panelHeight / 10);
+        diceRollerButton.setVisible(true);
+        diceRollingResult.setVisible(true);
+
+
+
+        diceRollerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Random rand = new Random();
+                int n = rand.nextInt(6) + 1;
+                diceRollingResult.setText(String.valueOf(n));
+            }
+        });
+
+        add(diceRollerButton);
+        add(diceRollingResult);
+    }
+
+
+    public void refreshSizes() {
+
+        panelWidth = (int) getSize().getWidth();
+        panelHeight = (int) getSize().getHeight();
+
+        xCoordinates[0] = (panelWidth / 10 * 3) - (ovalDiameter/2);
+        xCoordinates[1] = (panelWidth / 10 * 7) - (ovalDiameter/2);
+        xCoordinates[2] = (panelWidth / 10 * 3) - (ovalDiameter/2);
+        xCoordinates[3] = (panelWidth / 10 * 7) - (ovalDiameter/2);
+
+        yCoordinates[0] = (panelHeight / 10 * 3) - (ovalDiameter/2);
+        yCoordinates[1] = (panelHeight / 10 * 3)- (ovalDiameter/2);
+        yCoordinates[2] = (panelHeight / 10 * 7) - (ovalDiameter/2);
+        yCoordinates[3] = (panelHeight / 10 * 7) - (ovalDiameter/2);
+
+        this.ovalDiameter = panelWidth / 5;
+
+
+        diceRollerButton.setBounds(panelWidth / 10 , 5 , panelWidth/10 * 5, panelHeight / 10 );
+        diceRollingResult.setBounds(panelWidth / 10 * 8, 5, panelWidth / 10 * 3, panelHeight / 10);
+    }
+
+    public int[] getxCoordinates() {
+        return xCoordinates;
+    }
+
+    public void setxCoordinates(int[] xCoordinates) {
+        this.xCoordinates = xCoordinates;
+    }
+
+    public int[] getyCoordinates() {
+        return yCoordinates;
+    }
+
+    public void setyCoordinates(int[] yCoordinates) {
+        this.yCoordinates = yCoordinates;
+    }
+
+    public int getOvalDiameter() {
+        return ovalDiameter;
+    }
+
+    public void setOvalDiameter(int ovalDiameter) {
+        this.ovalDiameter = ovalDiameter;
+    }
+
+}
