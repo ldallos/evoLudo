cf195689a3fa95772f042aee4fe95495bfbcc122
aronnagy <aron.nagy1997@gmail.com>
2019-01-02 18:52:52 +0100

    Upgraded Dice rollings.

--- src/evoLudoMain/YardPanels.java
+++ src/evoLudoMain/YardPanels.java
@@ -1,9 +1,12 @@
 package evoLudoMain;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
 import java.util.Random;
 
 
@@ -15,7 +18,6 @@ public class YardPanels extends JPanel {
     private JLabel diceRollingResult = new JLabel();
     private int panelWidth = (int) getSize().getWidth();
     private int panelHeight = (int) getSize().getHeight();
-
 
     public void paint(Graphics g) {
         super.paint(g);
@@ -30,6 +32,8 @@ public class YardPanels extends JPanel {
 
         diceRollerButton.setBounds(panelWidth / 10 , 5 , panelWidth/10 * 5, panelHeight / 10 );
         diceRollingResult.setBounds(panelWidth / 10 * 8, 5, panelWidth / 10 * 3, panelHeight / 10);
+        diceRollingResult.setFont(new Font("Unicode",Font.PLAIN, 50));
+        //diceRollerButton.setVisible(true);
         diceRollerButton.setVisible(true);
         diceRollingResult.setVisible(true);
 
@@ -41,11 +45,30 @@ public class YardPanels extends JPanel {
                 Random rand = new Random();
                 int n = rand.nextInt(6) + 1;
                 diceRollingResult.setText(String.valueOf(n));
+                if(n == 1) {
+                    diceRollingResult.setText("\u2680");
+                }
+                if(n == 2) {
+                    diceRollingResult.setText("\u2681");
+                }
+                if(n == 3) {
+                    diceRollingResult.setText("\u2682");
+                }
+                if(n == 4) {
+                    diceRollingResult.setText("\u2683");
+                }
+                if(n == 5) {
+                    diceRollingResult.setText("\u2684");
+                }
+                if(n == 6) {
+                    diceRollingResult.setText("\u2685");
+                }
             }
         });
 
         add(diceRollerButton);
         add(diceRollingResult);
+
     }
 
 
